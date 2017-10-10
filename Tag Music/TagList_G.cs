7a9c0aa597824e27e3c91af0ac748bf56d6dd31f
Unknown <qjfrntop12@naver.com>
2017-10-11 00:04:37 +0900

    10_11 commit

---
+++ Tag Music/TagList_G.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tag_Music
+{
+    class TagList
+    {
+
+    }
+
+    class PlayList
+    {
+        class node
+        {
+            Object music;
+            node next;
+        }
+
+        public void AddTag(TagList tag)
+        {
+            //foreach()
+        }
+    }
+}
