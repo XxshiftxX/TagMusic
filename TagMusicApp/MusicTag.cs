7af990219d6592acd75f252e1ec61c8b3c8d92b4
Unknown <qjfrntop12@naver.com>
2017-12-21 23:49:08 +0900

    프로젝트 클린업 후 재시작

---
+++ TagMusicApp/MusicTag.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagMusicApp
+{
+    class MusicTag
+    {
+        private readonly string _tagName;
+
+        public readonly List<Music> Musics = new List<Music>();
+
+        public MusicTag(string name)
+        {
+            _tagName = name;
+        }
+    }
+}
